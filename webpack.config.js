23602ec4b6548cd8cd6c519bd869252b2b70bd79
gwanwoodev <devyeon1102@gmail.com>
2019-08-02 13:56:24 +0900

    add package.json script & add controller folder, ts file

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
-module.exports = {
-  entry: "./src/server.ts",
-  target: "node",
-  mode: "development",
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  },
-  module: {
-    rules: [{ test: /\.ts?$/, use: ["ts-loader"] }]
-  },
-  externals: [nodeExternals()]
-};
+module.exports = [
+  {
+    entry: "./src/server.ts",
+    target: "node",
+    output: {
+      filename: "server.js",
+      path: path.resolve(__dirname, "dist")
+    },
+    devtool: "source-map",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+      rules: [{ test: /\.ts?$/, use: ["ts-loader"] }]
+    },
+    externals: [nodeExternals()]
+  }
+];
