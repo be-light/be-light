51be324d22cbe3d6c5059cad3609d1f0610442c2
gwanwoodev <devyeon1102@gmail.com>
2019-08-12 17:24:05 +0900

    routePrv issue fix & update jwt test code

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as path from "path";
 import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
 import { Routes } from "./routes/belight.router";
 
 class App {
@@ -9,7 +10,6 @@ class App {
   constructor() {
     /* app settings */
     this.app = express();
-    this.routePrv.routes(this.app);
 
     /* set view folders, pug engine */
     this.app.set("views", path.join(__dirname, "views"));
@@ -19,8 +19,14 @@ class App {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
 
+    /* use cookie-parser */
+    this.app.use(cookieParser());
+
     /* setting default static folder */
     this.app.use(express.static("public_dist"));
+
+    /* routing settings */
+    this.routePrv.routes(this.app);
   }
 }
 
--- src/routes/belight.router.ts
+++ src/routes/belight.router.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import { User } from "../models/user.model";
+import * as jwt from "jsonwebtoken";
+import secretObj from "../config/jwt";
 
 export class Routes {
   public routes(app): void {
@@ -10,6 +13,27 @@ export class Routes {
       });
     });
 
-    app.route("/dummy").get((req: Request, res: Response) => {});
+    app.route("/api/auth/login").post((req: Request, res: Response) => {
+      let id = req.body.userId;
+
+      let token = jwt.sign(
+        {
+          userId: id
+        },
+        secretObj.secret,
+        {
+          expiresIn: "5m"
+        }
+      );
+
+      User.findOne({ where: { userId: id } }).then(user => {
+        //if (user.userPassword === "1234") {
+        res.cookie("user", token);
+        res.json({
+          token: token
+        });
+        //}
+      });
+    });
   }
 }
