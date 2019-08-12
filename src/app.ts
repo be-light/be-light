7dde46acbde6263dd833496194bbeee2a0ac297f
gwanwoodev <devyeon1102@gmail.com>
2019-08-12 10:47:25 +0900

    add sequelize config.json

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ class App {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
 
-    /* setting  default static folder */
+    /* setting default static folder */
     this.app.use(express.static("public_dist"));
   }
 }
