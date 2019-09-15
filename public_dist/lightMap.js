46a477a82139130f0f34f6ddf66d7939d11000c0
gwanwoodev <devyeon1102@gmail.com>
2019-09-15 13:35:27 +0900

    update google.map.marker

--- public_dist/lightMap.js
+++ public_dist/lightMap.js
@@ -1,11 +1,37 @@
 var map;
+var geocoder;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 37.5665,
+            lng: 126.9780
         },
         zoom: 8
     });
+
+    geocoder = new google.maps.Geocoder();
 }
+
+var search__form = document.querySelector(".search__form");
+var search__text = document.querySelector(".search__text");
+
+function findAddress(event) {
+    event.preventDefault();
+    geocoder.geocode({
+        'address': search__text.value
+    }, (results, status) => {
+        if (status === "ZERO_RESULTS") {
+            alert('Cannot find Address.');
+            return;
+        }
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        })
+    });
+
+}
+
+search__form.addEventListener("submit", findAddress)
