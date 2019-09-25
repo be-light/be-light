e202fc574dfbb06c1293809540d4690280379bf9
gwanwoodev <devyeon1102@gmail.com>
2019-09-25 09:55:08 +0900

    cloudflare ssl settings complete

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import "dotenv/config";
 import app from "./app";
 import sequelize from "./models/sequelize";
+import * as http from "http";
+import * as https from "https";
 
-/* Setting Server Port */
+/* Setting Servers */
 const PORT: any = process.env.SERVER_PORT || 3000;
 
 /* Sequelize Sync */
 sequelize.sync({ force: false });
 
 /* Server Start */
-app.listen(PORT, () => {
-  console.log(`Express Server Listening on Port ${PORT}`);
-});
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({}, app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
