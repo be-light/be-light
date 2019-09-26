81476a38c5132e3366505059847f081cb4641a5d
gwanwoodev <devyeon1102@gmail.com>
2019-09-26 09:09:46 +0900

    delete fullscreenControl & streetView Control from googlemaps

--- public_dist/lightMap.js
+++ public_dist/lightMap.js
@@ -4,60 +4,64 @@ var ZOOM = 12;
 
 /* Initialize Google Maps */
 function initMap() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(pos => {
-            map = new google.maps.Map(document.getElementById("map"), {
-                center: {
-                    lat: pos.coords.latitude,
-                    lng: pos.coords.longitude
-                },
-                zoom: ZOOM
-            });
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(pos => {
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        },
+        zoom: ZOOM,
+        fullscreenControl: false,
+        streetViewControl: false
+      });
 
-            /* Create Testing Marker */
-            var marker = new google.maps.Marker({
-                map: map,
-                position: {
-                    lat: pos.coords.latitude,
-                    lng: pos.coords.longitude
-                }
-            });
-        });
-    } else {
-        alert('This browser is not support geolocation.');
-    }
+      /* Create Testing Marker */
+      var marker = new google.maps.Marker({
+        map: map,
+        position: {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        }
+      });
+    });
+  } else {
+    alert("This browser is not support geolocation.");
+  }
 
-
-    geocoder = new google.maps.Geocoder();
+  geocoder = new google.maps.Geocoder();
 }
 
 var search__form = document.querySelector(".search__form");
 var search__text = document.querySelector(".search__text");
 
-
 /* Search Address */
 function findAddress(event) {
-    event.preventDefault();
-    geocoder.geocode({
-        'address': search__text.value
-    }, (results, status) => {
-        if (status === "ZERO_RESULTS") {
-            alert('Cannot find Address.');
-            return;
-        }
-        map.setCenter(results[0].geometry.location);
-        map.setZoom(ZOOM);
+  event.preventDefault();
+  geocoder.geocode(
+    {
+      address: search__text.value
+    },
+    (results, status) => {
+      if (status === "ZERO_RESULTS") {
+        alert("Cannot find Address.");
+        return;
+      }
+      map.setCenter(results[0].geometry.location);
+      map.setZoom(ZOOM);
 
-        const pos = results[0].geometry.location;
-        fetch(`/api/map/hosts?lat=${pos.lat()}&lng=${pos.lng()}`, {
-            method: 'GET',
-        }).then(response => {
-            return response.json();
-        }).then(result => {
-            console.log(result);
+      const pos = results[0].geometry.location;
+      fetch(`/api/map/hosts?lat=${pos.lat()}&lng=${pos.lng()}`, {
+        method: "GET"
+      })
+        .then(response => {
+          return response.json();
         })
-
-    });
+        .then(result => {
+          console.log(result);
+        });
+    }
+  );
 }
 
-search__form.addEventListener("submit", findAddress)
+search__form.addEventListener("submit", findAddress);
