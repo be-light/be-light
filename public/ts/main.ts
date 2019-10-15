2f61b20f3e6c5b26b7d2ec39dd0d964f32474f45
gwanwoodev <devyeon1102@gmail.com>
2019-10-15 23:45:18 +0900

    main test code

--- public/ts/main.ts
+++ public/ts/main.ts
@@ -1,2 +1,6 @@
 import "../scss/styles.scss";
+import { PUBLIC_USER } from "./login";
 import "./login.ts";
+
+console.log("Main");
+console.log(PUBLIC_USER);
--- public_dist/bundle.js
+++ public_dist/bundle.js
@@ -449,7 +449,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./public/scss/styles.scss\");\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _login_ts__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./login.ts */ \"./public/ts/login.ts\");\n\n\n\n//# sourceURL=webpack:///./public/ts/main.ts?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./public/scss/styles.scss\");\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _login__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./login */ \"./public/ts/login.ts\");\n\n\n\nconsole.log(\"Main\");\nconsole.log(_login__WEBPACK_IMPORTED_MODULE_1__[\"PUBLIC_USER\"]);\n\n//# sourceURL=webpack:///./public/ts/main.ts?");
 
 /***/ })
 
