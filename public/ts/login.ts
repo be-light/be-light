9d041a869d12bf5de6cc194221d5a43faaefd04c
gwanwoodev <devyeon1102@gmail.com>
2019-10-15 12:40:13 +0900

    login Modal footer done

--- public/ts/login.ts
+++ public/ts/login.ts
@@ -1,15 +1,74 @@
 /* Get Element */
+const body: HTMLElement = document.querySelector("body");
+
 const modal: HTMLElement = document.getElementById("loginModal");
 const modalBtn: HTMLElement = document.querySelector(".header__menu--login");
+const closeBtn: HTMLElement = document.querySelector(".closeBtn");
+const contents: HTMLElement = document.querySelector("#contents");
+
+const loginForm: HTMLElement = document.querySelector(".loginForm");
+const loginFooter: HTMLElement = document.querySelector(".login__footer");
+const loginLink: HTMLElement = document.querySelector(".login--link");
+
+const registerFooter: HTMLElement = document.querySelector(".register__footer");
+const registerForm: HTMLElement = document.querySelector(".registerForm");
+const registerLink: HTMLElement = document.querySelector(".register--link");
+
+const navTitle: HTMLElement = document.querySelector(
+  ".modal__nav__header--title"
+);
+
+/* Hide Register Form */
+registerForm.style.display = "none";
+registerFooter.style.display = "none";
+
+/* Open Register Form Event */
+registerLink.addEventListener("click", () => {
+  loginForm.style.display = "none";
+  registerForm.style.display = "block";
+  registerLink.innerText = "Login";
+  navTitle.innerText = "Register";
+  loginFooter.style.display = "none";
+  registerFooter.style.display = "block";
+});
+
+/* Open Login Event */
+loginLink.addEventListener("click", () => {
+  initializeModal();
+});
 
 /* Open Modal Event */
 modalBtn.addEventListener("click", () => {
   modal.style.display = "block";
+  contents.style.opacity = "0.5";
 });
 
 /* Click Outside and Close */
 window.addEventListener("click", evt => {
-  if (evt.target === modal) modal.style.display = "none";
+  if (evt.target === body) {
+    closeModalEvent();
+    initializeModal();
+  }
 });
 
 /* Close Modal Event */
+closeBtn.addEventListener("click", () => {
+  closeModalEvent();
+  initializeModal();
+});
+
+/* Close Modal Event Function */
+const closeModalEvent = () => {
+  modal.style.display = "none";
+};
+
+/* Initialize Modal Function */
+const initializeModal = () => {
+  contents.style.opacity = "1.0";
+  registerForm.style.display = "none";
+  registerLink.innerText = "Register now";
+  navTitle.innerText = "Login";
+  loginForm.style.display = "block";
+  registerFooter.style.display = "none";
+  loginFooter.style.display = "block";
+};
