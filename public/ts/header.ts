9c4de4a51b4be70947287111be531ef01537ab79
gwanwoodev <devyeon1102@gmail.com>
2019-10-16 14:11:07 +0900

    create testing method header.ts

--- public/ts/header.ts
+++ public/ts/header.ts
@@ -1,7 +1,13 @@
 class BeLightHeader {
   public LOGO: HTMLImageElement;
+  public searchButton: HTMLButtonElement;
+  public startDate: HTMLInputElement;
+  public endDate: HTMLInputElement;
+  public bagCount: string;
+
   constructor() {
     this.LOGO = document.querySelector(".header__left--logo");
+    this.searchButton = document.querySelector(".intro__searchform--submitbtn");
 
     /* Add All EventListeners */
     this.addEvents();
@@ -11,6 +17,17 @@ class BeLightHeader {
     /* Click Move Event */
     this.LOGO.addEventListener("click", () => {
       location.href = "/";
+    });
+
+    /* Click Seach Button Event */
+    this.searchButton.addEventListener("click", () => {
+      this.startDate = document.querySelector(".startDate");
+      this.endDate = document.querySelector(".endDate");
+      this.bagCount = document.querySelector("input[name=bagCount]").innerHTML;
+
+      console.log(this.startDate);
+      console.log(this.endDate);
+      console.log(this.bagCount);
     });
   };
 }
