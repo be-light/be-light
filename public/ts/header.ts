8283868e60858419be96e677460929a01eeaddb4
gwanwoodev <devyeon1102@gmail.com>
2019-10-16 16:20:29 +0900

    header.ts test done

--- public/ts/header.ts
+++ public/ts/header.ts
@@ -3,11 +3,14 @@ class BeLightHeader {
   public searchButton: HTMLButtonElement;
   public startDate: HTMLInputElement;
   public endDate: HTMLInputElement;
-  public bagCount: string;
+  public bagCount: HTMLInputElement;
 
   constructor() {
     this.LOGO = document.querySelector(".header__left--logo");
     this.searchButton = document.querySelector(".intro__searchform--submitbtn");
+    this.startDate = document.querySelector(".startDate");
+    this.endDate = document.querySelector(".endDate");
+    this.bagCount = document.querySelector("input[name=bagCount]");
 
     /* Add All EventListeners */
     this.addEvents();
@@ -21,13 +24,9 @@ class BeLightHeader {
 
     /* Click Seach Button Event */
     this.searchButton.addEventListener("click", () => {
-      this.startDate = document.querySelector(".startDate");
-      this.endDate = document.querySelector(".endDate");
-      this.bagCount = document.querySelector("input[name=bagCount]").innerHTML;
-
-      console.log(this.startDate);
-      console.log(this.endDate);
-      console.log(this.bagCount);
+      console.log(this.startDate.value);
+      console.log(this.endDate.value);
+      console.log(this.bagCount.value);
     });
   };
 }
