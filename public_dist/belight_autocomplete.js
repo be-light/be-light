d4364f36346bfef937589032b477bf1cccfd779c
gwanwoodev <devyeon1102@gmail.com>
2019-10-17 14:13:24 +0900

    styling autocomplete input

--- public_dist/belight_autocomplete.js
+++ public_dist/belight_autocomplete.js
@@ -1,5 +1,6 @@
 var input;
 var autocomplete;
+
 document.addEventListener("DOMContentLoaded", function(event) {
   input = document.querySelector(".input__place");
   autocomplete = new google.maps.places.Autocomplete(input, {
