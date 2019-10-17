2bd40e62245c65074af29b32da188304e85c4306
gwanwoodev <devyeon1102@gmail.com>
2019-10-17 14:37:35 +0900

    showCurrentPosition, setDefaultPosition done

--- public_dist/lightMap.js
+++ public_dist/lightMap.js
@@ -3,8 +3,15 @@ var geocoder;
 var ZOOM = 11;
 var prev_infowindow = false;
 
-/* Initialize Google Maps */
-function initMap() {
+function getQueryString() {
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  var dropPlace = url.searchParams.get("dropPlace");
+  return dropPlace;
+}
+
+/* Don't have QueryStrings then, set Default GoogleMaps Position*/
+function setDefaultPosition() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(pos => {
       map = new google.maps.Map(document.getElementById("map"), {
@@ -17,12 +24,40 @@ function initMap() {
         streetViewControl: false,
         mapTypeControl: false
       });
+
+      var latLng = new google.maps.LatLng(
+        pos.coords.latitude,
+        pos.coords.longitude
+      );
+
+      showCurrentPosition(latLng);
     });
   } else {
     alert("This browser is not support geolocation.");
   }
+}
 
-  geocoder = new google.maps.Geocoder();
+/* Show Current Position with Marker */
+function showCurrentPosition(latLng) {
+  var marker = new google.maps.Marker({
+    position: latLng,
+    title: "내 위치",
+    map: map,
+    draggable: false
+  });
+}
+
+/* Initialize Google Maps */
+function initMap() {
+  var dropPlace = getQueryString();
+
+  if (dropPlace) {
+    alert("구글맵");
+  } else {
+    setDefaultPosition();
+  }
+
+  //geocoder = new google.maps.Geocoder();
 
   /*const input = document.getElementById("searchTextField");
   let autocomplete = new google.maps.places.Autocomplete(input, {
