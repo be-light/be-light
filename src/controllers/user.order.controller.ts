f1722d6f4ccf592916c9bf19690cb92fa905b6b9
gwanwoodev <devyeon1102@gmail.com>
2019-10-23 09:59:13 +0900

    getUserOrderList - Sequelize Database Error fix

--- src/controllers/user.order.controller.ts
+++ src/controllers/user.order.controller.ts
@@ -42,7 +42,6 @@ a.HostIdx as hostIdx,
 a.statusCode as statusCode,
 a.gHostIdx as gHostIdx,
 a.itemCount as itemCount,
-a.qrCode as qrCode,
 
 b.hostAddress as hostaddress,
 b.hostPostalCode as hostPostalCode,
